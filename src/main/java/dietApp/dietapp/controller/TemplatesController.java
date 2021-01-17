13aa271535f41aff4867c898c111a74db3ee112e
Adam Gwóźdź <70854700+adamgwozdz00@users.noreply.github.com>
2021-01-17 22:15:21 +0100

    almost done

--- src/main/java/dietApp/dietapp/controller/TemplatesController.java
+++ src/main/java/dietApp/dietapp/controller/TemplatesController.java
@@ -1,16 +1,12 @@
 package dietApp.dietapp.controller;
 
-import dietApp.dietapp.model.User;
 import dietApp.dietapp.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping("/")
