e1ed3957b7ff87b7dc661cef3b515ee3b1977479
Adam Gwóźdź <70854700+adamgwozdz00@users.noreply.github.com>
2021-01-18 13:18:13 +0100

    fixed

--- src/main/java/dietApp/dietapp/client/FoodApiClient.java
+++ src/main/java/dietApp/dietapp/client/FoodApiClient.java
@@ -7,8 +7,8 @@ import org.springframework.web.client.RestTemplate;
 public class FoodApiClient {
 
     private static String API_URL = "https://api.edamam.com/api/nutrition-data?nutrition-type=logging";
-    private static String API_ID = "677c950a";
-    private static String API_KEY ="181299622f631740f83ec712e5706e09";
+    private static String API_ID = "";
+    private static String API_KEY ="";
     private RestTemplate restTemplate = new RestTemplate();
 
     public Object getFoodAsString(String foodName){
